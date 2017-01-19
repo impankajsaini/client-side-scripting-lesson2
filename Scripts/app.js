22eb454fd2fe0b99be254e4897b2757ecff56133
impankajsaini <impankaj.saini.1947@gmail.com>
2017-01-19 00:55:45 -0500

    Added button

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,2 +1,10 @@
 /* custom javascript goes here */
 console.log("app started!");
+
+var clickMeButton = document.getElementById("clickMeButton");
+
+clickMeButton.addEventListener("click", CLick);
+
+function CLick() {
+    console.log("clicked!");
+}
